e413d0b7c30d4af21a19d0611d8a6af09ecd6dcd
Ashlei Perry <ashlei.perry@gmail.com>
2017-11-06 09:18:34 -0500

    set up app to have react

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 import './app.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Popup from './components/Popup';
+
+ReactDOM.render(
+  <Popup name= "Taylor Swift"/>,
+  document.getElementById('app')
+);
